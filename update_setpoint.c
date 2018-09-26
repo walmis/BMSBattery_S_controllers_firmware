c57e268318a88e3ee6c240c634faa706bc1cb6ca
stancecoke <graf.koks@freenet.de>
2018-09-26 21:06:33 +0200

    bugfix

--- update_setpoint.c
+++ update_setpoint.c
@@ -316,7 +316,7 @@ uint32_t PI_control (uint16_t ist, uint16_t soll)
   if (float_i>255)float_i=255;
   if (float_i<0)float_i=0;
   if (float_p+float_i+5>float_dc)float_dc+=5;
-  else if  (float_p+float_i-5>float_dc)float_dc-=5;
+  else if  (float_p+float_i-5<float_dc)float_dc-=5;
   else float_dc=float_p+float_i;
   if (float_dc>255)float_dc=255;
   if (float_dc<0)float_dc=0;
